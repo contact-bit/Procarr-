db8258814c76f0b87d03783c7fe36023727cad3f
Massme <contact@hdconnects.com>
2026-04-22 19:58:33 +0200

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import './styles/global.css';
-
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>,
 );
